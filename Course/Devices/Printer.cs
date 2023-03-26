dd932e854d9a26164955950aa014c5f8228185d7
Vitor Bahir <vitorb82014@gmail.com>
2023-03-26 18:01:56 -0300

    Initial implementation

---
+++ Course/Devices/Printer.cs
@@ -0,0 +1,15 @@
+﻿namespace Course.Devices
+{
+    class Printer : Device
+    {
+        public override void ProcessDoc(string document)
+        {
+            Console.WriteLine("Printer processing: " + document);
+        }
+
+        public void Print(string document)
+        {
+            Console.WriteLine("Printer print " + document);
+        }
+    }
+}
